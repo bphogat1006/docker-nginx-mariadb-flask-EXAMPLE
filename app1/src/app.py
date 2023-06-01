d441599d2080a68b9db72d26d3b403e1d4c4c717
Bhavya Phogat <b.phogat1925@gmail.com>
2023-06-01 17:40:29 -0400

    docker compose working for nginx and 2 flask apps

---
+++ app1/src/app.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, Response
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    headers = ''
+    for k,v in request.headers.items():
+        headers += f'<br>{k}: {v}'
+    return Response('hi this is app1<br>'+headers)
